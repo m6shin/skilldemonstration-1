6cecdbd3ddb41cf3649029687322dd155eae16e6
m6shin <m6shin@ucsd.edu>
2022-02-04 14:22:30 -0800

    lib + failed test

--- SkillDemonstration1.java
+++ SkillDemonstration1.java
@@ -1,1 +1,5 @@
-public class SkillDemonstration1 {}
+public class SkillDemonstration1 {
+    public static int subtract(int p, int q) {
+		return p - q;
+	}
+}
---
+++ SkillDemonstration1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstration1Test {
+    @Test
+	public void subtraction() {
+		assertEquals(200, SkillDemonstration1.subtract(9, 9));
+	}
+}
